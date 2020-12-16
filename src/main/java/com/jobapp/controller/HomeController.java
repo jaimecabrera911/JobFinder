bce0b399517d47ad08dbd583939b0c84b13c4ed7
Desarrollador Web 6 <desarrolladorweb6@croydon.com.co>
2020-12-16 11:28:31 -0500

    Agregar informacion al modelo para poder desplegarla en la vista

--- src/main/java/com/jobapp/controller/HomeController.java
+++ src/main/java/com/jobapp/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.jobapp.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+    	
+    	model.addAttribute("mensaje", "Bienvenido a Empleos App");
+    	model.addAttribute("fecha",new Date());
         return "home";
     }
 
