b1e9373d1a96f287e8ec428933a799b0f075ca76
Desarrollador Web 6 <desarrolladorweb6@croydon.com.co>
2020-12-16 12:44:12 -0500

    Agregar un tipo de dato Vacante al modelo y desplegarlo en la vista

--- src/main/java/com/jobapp/controller/HomeController.java
+++ src/main/java/com/jobapp/controller/HomeController.java
@@ -1,50 +1,66 @@
 package com.jobapp.controller;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jobapp.model.Vacante;
+
 @Controller
 @RequestMapping
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(Model model) {
+	@GetMapping("/")
+	public String index(Model model) {
 
-        /*
-        model.addAttribute("mensaje", "Bienvenido a Empleos App");
-    	model.addAttribute("fecha",new Date());
-         */
-        String nombre = "Desarrollador Web";
-        Date fechaPub = new Date();
-        double salario = 3500000;
-        boolean vigente = true;
+		/*
+		 * model.addAttribute("mensaje", "Bienvenido a Empleos App");
+		 * model.addAttribute("fecha",new Date());
+		 */
+		String nombre = "Desarrollador Web";
+		Date fechaPub = new Date();
+		double salario = 3500000;
+		boolean vigente = true;
 
-        model.addAttribute("nombre", nombre);
-        model.addAttribute("fechaPub", fechaPub);
-        model.addAttribute("salario", salario);
-        model.addAttribute("vigente", vigente);
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fechaPub", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
 
-        return "home";
-    }
-    
-    @GetMapping("/listado")
-    public String lista(Model model){
-    	List<String> lista=new LinkedList<>();
-    	lista.add("Desarrollador Web");
-    	lista.add("Desarrollador Jr");
-    	lista.add("Desarrollador Senior");
-    	lista.add("Desarrollador Full Stack");
-    	model.addAttribute("empleos", lista);
-    	return "lista";
-    }
+		return "home";
+	}
+
+	@GetMapping("/listado")
+	public String lista(Model model) {
+		List<String> lista = new LinkedList<>();
+		lista.add("Desarrollador Web");
+		lista.add("Desarrollador Jr");
+		lista.add("Desarrollador Senior");
+		lista.add("Desarrollador Full Stack");
+		model.addAttribute("empleos", lista);
+		return "lista";
+	}
+
+	@GetMapping("/detalle")
+	public String vacante(Model model) {
+
+		Vacante vacante = new Vacante();
+		vacante.setId(1L);
+		vacante.setNombre("Desarrollador Full Stack");
+		vacante.setDescripcion(
+				"Encargado de manejar cada uno de los aspectos relacionados con la creación y el mantenimiento de una aplicación web. "
+						+ "Para ello es fundamental que el desarrollador Full Stack tenga conocimientos en desarrollo Front-End y Back-End.");
+		vacante.setSalario(3500000.0);
+		vacante.setFecha(new Date());
+
+		model.addAttribute("vacante", vacante);
+
+		return "detalle";
+	}
 
 }
