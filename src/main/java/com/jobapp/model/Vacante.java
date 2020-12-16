70c2101e1eadb220169c74dd7077bce93937397d
Desarrollador Web 6 <desarrolladorweb6@croydon.com.co>
2020-12-16 12:23:41 -0500

    "Crear la clase de modelo Vacante para representar una oferta de trabajo"

---
+++ src/main/java/com/jobapp/model/Vacante.java
@@ -0,0 +1,82 @@
+package com.jobapp.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Vacante {
+	
+	private Long id;
+	
+	private String nombre;
+	
+	private String descripcion;
+	
+	private Date fecha;
+	
+	private Double salario;
+	
+	
+
+	public Vacante() {
+		super();
+	}
+
+	public Vacante(Long id, String nombre, String descripcion, Date fecha, Double salario) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.fecha = fecha;
+		this.salario = salario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Vacante [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
+				+ ", salario=" + salario + "]";
+	}
+	
+	
+}
